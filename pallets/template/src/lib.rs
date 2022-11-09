a97ada901f6a260577c98829cf41cb9b9b026614
Dang Huy Hoang <dang_huy.hoang@student.unsw.edu.au>
2022-11-09 16:29:53 +0700

    add

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -38,6 +38,11 @@ pub mod pallet {
 	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
 	pub type Something<T> = StorageValue<_, u32>;
 
+	#[pallet::storage]
+	pub type Number<T: Config> = StorageMap<_,Blake2_128Concat,
+					T::AccountId,
+					u32,
+					ValueQuery, >;
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/main-docs/build/events-errors/
 	#[pallet::event]
@@ -80,6 +85,39 @@ pub mod pallet {
 			Ok(())
 		}
 
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		pub fn put_number(origin: OriginFor<T>, number: u32) -> DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/main-docs/build/origins/
+			let who = ensure_signed(origin)?;
+
+			// Update storage.
+			<Number<T>>::insert(who.clone() , number);
+
+			// Emit an event.
+			Self::deposit_event(Event::SomethingStored(number, who));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
+
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		pub fn remove_number(origin: OriginFor<T>) -> DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/main-docs/build/origins/
+			let who = ensure_signed(origin)?;
+
+			// Update storage.
+			<Number<T>>::take(who.clone());
+
+			// Emit an event.
+			// Self::deposit_event(Event::SomethingStored(number, who));
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
+
+	
 		/// An example dispatchable that may throw a custom error.
 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
 		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
